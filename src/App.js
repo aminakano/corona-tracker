7975e06fa8cb7751c6052892b504f85655a38715
Ami Nakano <ami.nakano@outlook.com>
2020-12-24 22:10:00 +0100

    tutorials done

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
+import coronaImg from "./images/image.png";
+
 export class App extends Component {
   state = {
-    data: {}
+    data: {},
+    country: ""
   }
 
   async componentDidMount() {
     const fetchedData = await fetchData();
+
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <Cards data={data}/>
-        <CountryPicker />
-        <Chart />
+        <img className={styles.image} src={coronaImg} alt="Covid-19"/>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
-    )
+    );
   }
 }
 
